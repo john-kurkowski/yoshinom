7c4ebd95205230e1ba80c06e8b698056a72c5075
John Kurkowski <john.kurkowski@gmail.com>
2014-03-16 23:42:31 -0700

    Rewrite all !^/assets/ to /index.html to preserve SPA
    Compress and maxAge assets.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var connect = require('connect'),
-    http = require('http'),
+    modRewrite = require('connect-modrewrite');
+
+var staticMaxAge = 1000 * 60 * 60 * 2,
     app;
 
 app = connect()
-  .use(connect.static('public'));
-
-http.createServer(app).listen(process.env.PORT || 3000);
+  .use(modRewrite([
+    '!^/assets/ /index.html'
+  ]))
+  .use(connect.compress())
+  .use(connect.static('public', { maxAge: staticMaxAge }))
+  .listen(process.env.PORT || 3000);
