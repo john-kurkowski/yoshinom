56b5684e8e06b78412371280fc344432ee749124
John Kurkowski <john.kurkowski@gmail.com>
2014-12-20 19:31:16 -0800

    remove unused property

--- app/components/yoshinom-venue.js
+++ app/components/yoshinom-venue.js
@@ -4,7 +4,6 @@ export default Ember.Component.extend({
 
   venue: null,
   itemRoute: "",
-  areImagesLoaded: false,
 
   assertVenue: function() {
     Ember.assert(this.get('venue'), 'No venue passed to yoshinom-venue');
