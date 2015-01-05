8acdfee4a2b347e23c742590e2d0bbd17c886e3a
John Kurkowski <john.kurkowski@gmail.com>
2015-01-04 16:31:05 -0800

    Tell prerender.io when we've really finished loading, when loading spinners are gone

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  needs: ['food', 'cocktails'],
+
+  setPrerenderReady: function() {
+    var controllers = [
+      this.get('controllers.food'),
+      this.get('controllers.cocktails')
+    ];
+    var isRequestedPageSpinnerFree = controllers.anyBy('areImagesLoaded');
+    if (isRequestedPageSpinnerFree) {
+      Ember.run.scheduleOnce('afterRender', function() {
+        window.prerenderReady = true;
+      });
+    }
+  }.observes(
+    'controllers.food.areImagesLoaded',
+    'controllers.cocktails.areImagesLoaded'
+  ).on('init'),
+
   isDrinking: function() {
     return /cocktails/.test(this.get('currentPath'));
   }.property('currentPath')
--- app/mixins/venue-array-controller.js
+++ app/mixins/venue-array-controller.js
@@ -18,7 +18,7 @@ export default Ember.Mixin.create({
   queryParams: ['q', 's'],
 
   areImagesLoaded: function() {
-    return this.every(function(item) { return item.get('isImageLoaded'); });
-  }.property('content.@each.isImageLoaded')
+    return this.get('content.length') && this.every(function(item) { return item.get('isImageLoaded'); });
+  }.property('content.length', 'content.@each.isImageLoaded')
 
 });
