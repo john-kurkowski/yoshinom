8611f4c421ec83a1013475d9533be00a9ddaf303
John Kurkowski <john.kurkowski@gmail.com>
2015-08-23 10:19:32 -0700

    Update to latest GA tag

--- vendor/analytics.js
+++ vendor/analytics.js
@@ -3,7 +3,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-49300538-1', 'yoshinom.com');
+ga('create', 'UA-49300538-1', 'auto');
 ga('require', 'displayfeatures');
 ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
