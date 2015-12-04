c8587079880111ea32eb85cdd52c9d68be0080ea
John Kurkowski <john.kurkowski@gmail.com>
2015-12-03 19:15:05 -0800

    Fix and actually test error substates
    * Handle errors at the item (detail) level with a specific inline message. Bubble up to the application level with a very broad message.
    * Delete any handling in between.
    * Reintroduce SinonJS to stub Ember.Test.adapter's default of dying on any Route.model error, letting the test case enter the error substate

--- app/cocktails-error/controller.js
+++
@@ -1,1 +0,0 @@
-../controllers/section-error.js
--- tests/acceptance/error-test.js
+++ tests/acceptance/error-test.js
@@ -1,24 +1,60 @@
+import Ember from 'ember';
 import moduleForAcceptance from '../helpers/module-for-acceptance';
-import QUnit from 'qunit';
+import sinon from 'sinon';
+import { test } from 'qunit';
 
 import { createSheets} from 'yoshinom/mirage/scenarios/default';
 
 moduleForAcceptance('Acceptance | error', {
+  beforeEach() {
+    this.sandbox = sinon.sandbox.create();
+
+    /**
+     * Override Ember.Test's default failure on uncaught errors.
+     *
+     * @private
+     */
+    this.sandbox.stub(Ember.Test.adapter, 'exception');
+  },
+
+  afterEach() {
+    this.sandbox.restore();
+  }
 });
 
-// TODO: Ember fails any test that triggers the default error handler. How to
-//       work around?
+test('section page error', function(assert) {
+  assert.expect(3);
 
-QUnit.skip('section page error', function(/*assert*/) {
+  // Don't createSheets to force fatal error.
+  //   createSheets(server);
+
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+
+    const defaultRoute = 'food';
+    assert.equal(currentRouteName(), `${defaultRoute}_error`);
+
+    const errorMessage = find('.error').text().trim();
+    assert.ok(errorMessage.indexOf('nom nom') >= 0, `Error message: "${errorMessage}"`);
+  });
 });
 
-QUnit.skip('detail page 404', function(assert) {
+test('detail page 404', function(assert) {
+  assert.expect(3);
+
   createSheets(server);
 
   const unlikelyDetailRoute = '/food/unlikely-restaurant-name';
+
   visit(unlikelyDetailRoute);
 
   andThen(function() {
     assert.equal(currentURL(), unlikelyDetailRoute);
+    assert.equal(currentRouteName(), 'food.item_error');
+
+    const errorMessage = find('.error').text().trim();
+    assert.ok(errorMessage.indexOf('couldn\'t find that one') >= 0, `Error message: "${errorMessage}"`);
   });
 });
