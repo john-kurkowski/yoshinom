27ac1cc930440277c1479f7f77d1b2603e478486
John Kurkowski <john.kurkowski@gmail.com>
2016-03-05 16:57:30 -0800

    Upgrade from fork to ember-cli-sass-lint 1.x

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,13 +1,9 @@
 /*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    sassLint: {
-      configPath: '.sass-lint.yml'
-    },
-
+  const app = new EmberApp(defaults, {
     sassOptions: {
       includePaths: [
         'app',
