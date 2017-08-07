b3db4ddb591d0da473e7639e32f9239cd834ca37
John Kurkowski <john.kurkowski@gmail.com>
2017-08-07 00:19:32 -0700

    Switch CI to headless Chrome

--- testem.js
+++ testem.js
@@ -3,10 +3,15 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Firefox'
+    'Chrome',
   ],
   launch_in_dev: [
     'Chrome',
-    'Firefox',
-  ]
+  ],
+  browser_args: {
+    'Chrome': {
+      mode: 'ci',
+      args: [ '--headless', '--disable-gpu', '--remote-debugging-port=9222' ],
+    },
+  },
 };
