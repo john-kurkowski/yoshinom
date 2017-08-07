2309b05ff48a2420461fcdde2844fef9e67d5ceb
John Kurkowski <john.kurkowski@gmail.com>
2017-08-07 00:36:57 -0700

    fixup! Switch CI to headless Chrome

--- testem.js
+++ testem.js
@@ -7,5 +7,11 @@ module.exports = {
   ],
   launch_in_dev: [
     'Chrome',
-  ]
+  ],
+  browser_args: {
+    'Chrome': {
+      mode: 'ci',
+      args: [ '--headless', '--disable-gpu', '--remote-debugging-port=9222' ],
+    },
+  },
 };
